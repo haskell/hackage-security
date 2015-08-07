55ffa60e204ae62f6a2d79a0c2c84d9fe0671972
Edsko de Vries <edsko@well-typed.com>
2015-08-07 10:55:36 +0100

    Avoid use of unsafeCoerce in ghc >= 7.8

--- hackage-security/src/Hackage/Security/Util/Checked.hs
+++ hackage-security/src/Hackage/Security/Util/Checked.hs
@@ -1,48 +1,60 @@
--- | Checked exceptions
 {-# LANGUAGE CPP #-}
-#if __GLASGOW_HASKELL__ >= 710
-{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
+
+#if __GLASGOW_HASKELL__ >= 708
+{-# LANGUAGE RoleAnnotations     #-}
+{-# LANGUAGE IncoherentInstances #-}
 #endif
+
+-- | Checked exceptions
 module Hackage.Security.Util.Checked (
-    Throws(..)
-  , checkIO
-  , rethrowUnchecked
+    Throws
+  , unthrow
+    -- ** Base exceptions
+  , throwChecked
   , catchChecked
   , handleChecked
   , tryChecked
+  , checkIO
   , throwUnchecked
   , internalError
   ) where
 
-import Control.Exception
+import Control.Exception (Exception, IOException)
+import qualified Control.Exception as Base
+
+#if __GLASGOW_HASKELL__ >= 708
+import GHC.Prim (coerce)
+#else
 import Unsafe.Coerce (unsafeCoerce)
+#endif
 
 {-------------------------------------------------------------------------------
-  Checked exceptions
+  Basic infrastructure
 -------------------------------------------------------------------------------}
 
 -- | Checked exceptions
 class Throws e where
-  throwChecked :: e -> IO a
 
--- | Wrap an action that may throw a checked exception
---
--- This is used internally in 'rethrowUnchecked' to avoid impredicative
--- instantiation of the type of 'unsafeCoerce'.
-newtype Wrap e a = Wrap (Throws e => IO a)
+#if __GLASGOW_HASKELL__ >= 708
+type role Throws representational
+#endif
 
--- | Rethrow checked exceptions as unchecked (regular) exceptions
-rethrowUnchecked :: forall e a. (Throws e => IO a) -> (Exception e => IO a)
-rethrowUnchecked act = aux act throwIO
-  where
-    aux :: (Throws e => IO a) -> ((e -> IO a) -> IO a)
-    aux = unsafeCoerce . Wrap
+unthrow :: proxy e -> (Throws e => a) -> a
+unthrow _ = unWrap . coerceWrap . Wrap
+
+{-------------------------------------------------------------------------------
+  Base exceptions
+-------------------------------------------------------------------------------}
+
+-- | Throw a checked exception
+throwChecked :: (Exception e, Throws e) => e -> IO a
+throwChecked = Base.throwIO
 
 -- | Catch a checked exception
---
--- This is the only way to discharge a 'Throws' type class constraint.
-catchChecked :: Exception e => (Throws e => IO a) -> (e -> IO a) -> IO a
-catchChecked = catch . rethrowUnchecked
+catchChecked :: forall a e. Exception e
+             => (Throws e => IO a) -> (e -> IO a) -> IO a
+catchChecked act = Base.catch (unthrow (Proxy :: Proxy e) act)
 
 -- | 'catchChecked' with the arguments reversed
 handleChecked :: Exception e => (e -> IO a) -> (Throws e => IO a) -> IO a
@@ -52,17 +64,39 @@ handleChecked act handler = catchChecked handler act
 tryChecked :: Exception e => (Throws e => IO a) -> IO (Either e a)
 tryChecked act = catchChecked (Right <$> act) (return . Left)
 
+-- | Rethrow IO exceptions as checked exceptions
+checkIO :: Throws IOException => IO a -> IO a
+checkIO = Base.handle $ \(ex :: IOException) -> throwChecked ex
+
 -- | Throw an unchecked exception
 --
 -- This is just an alias for 'throw', but makes it evident that this is a very
 -- intentional use of an unchecked exception.
 throwUnchecked :: Exception e => e -> IO a
-throwUnchecked = throwIO
+throwUnchecked = Base.throwIO
 
 -- | Variation on 'throwUnchecked' for internal errors
 internalError :: String -> IO a
 internalError = throwUnchecked . userError
 
--- | Rethrow IO exceptions as checked exceptions
-checkIO :: Throws IOException => IO a -> IO a
-checkIO = handle $ \(ex :: IOException) -> throwChecked ex
+{-------------------------------------------------------------------------------
+  Auxiliary definitions (not exported)
+-------------------------------------------------------------------------------}
+
+-- | Wrap an action that may throw a checked exception
+--
+-- This is used internally in 'unthrow' to avoid impredicative
+-- instantiation of the type of 'coerce'/'unsafeCoerce'.
+newtype Wrap e a = Wrap { unWrap :: Throws e => a }
+
+coerceWrap :: Wrap e a -> Wrap (Catch e) a
+#if __GLASGOW_HASKELL__ >= 708
+coerceWrap = coerce
+#else
+coerceWrap = unsafeCoerce
+#endif
+
+data Proxy a = Proxy
+
+newtype Catch a = Catch a
+instance Throws (Catch e) where
