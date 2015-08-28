67fd9d8d9b66d8f03ba5fa58fb6ed7691a592088
Edsko de Vries <edsko@well-typed.com>
2015-08-28 16:25:25 +0100

    Whoops, missing file in TestSuite

---
+++ hackage-security/tests/TestSuite/Util/StrictMVar.hs
@@ -0,0 +1,26 @@
+module TestSuite.Util.StrictMVar (
+    MVar -- opaque
+  , newMVar
+  , CC.withMVar
+  , modifyMVar
+  , modifyMVar_
+  ) where
+
+import Control.Concurrent (MVar)
+import Control.Exception
+import qualified Control.Concurrent as CC
+
+newMVar :: a -> IO (MVar a)
+newMVar x = CC.newMVar =<< evaluate x
+
+modifyMVar :: MVar a -> (a -> IO (a, b)) -> IO b
+modifyMVar mv f = CC.modifyMVar mv $ \old -> do
+    (new, ret) <- f old
+    new' <- evaluate new
+    return (new', ret)
+
+modifyMVar_ :: MVar a -> (a -> IO a) -> IO ()
+modifyMVar_ mv f = modifyMVar mv (returnUnit . f)
+  where
+    returnUnit :: IO a -> IO (a, ())
+    returnUnit = fmap $ \a -> (a, ())
