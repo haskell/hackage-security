haskell/hackage-security

commit 9b6adacb98a363708c1dd4e517ef6fc420fea8a0

Switch to cryptohash's module-based API

hackage-security/src/Hackage/Security/Key.hs
@@ -29,9 +29,10 @@ import Control.Monad
 import Data.Functor.Identity
 import Data.Typeable (Typeable)
 import Text.JSON.Canonical
-import qualified Crypto.Hash          as CH
+import qualified Crypto.Hash.SHA256   as SHA256
 import qualified Crypto.Sign.Ed25519  as Ed25519
 import qualified Data.ByteString      as BS
+import qualified Data.ByteString.Base16 as Base16
 import qualified Data.ByteString.Lazy as BS.L
 
 #if !MIN_VERSION_base(4,7,0)
@@ -160,7 +161,8 @@ class HasKeyId key where
 instance HasKeyId PublicKey where
   keyId = KeyId
         . show
-        . (CH.hashlazy :: BS.L.ByteString -> CH.Digest CH.SHA256)
+        . Base16.encode
+        . SHA256.hashlazy
         . renderCanonicalJSON
         . runIdentity
         . toJSON

hackage-security/src/Hackage/Security/TUF/FileInfo.hs
@@ -14,8 +14,9 @@ module Hackage.Security.TUF.FileInfo (
 
 import Prelude hiding (lookup)
 import Data.Map (Map)
-import qualified Crypto.Hash          as CH
+import qualified Crypto.Hash.SHA256   as SHA256
 import qualified Data.Map             as Map
+import qualified Data.ByteString.Base16 as Base16
 import qualified Data.ByteString.Lazy as BS.L
 
 import Hackage.Security.JSON
@@ -58,7 +59,7 @@ fileInfo :: BS.L.ByteString -> FileInfo
 fileInfo bs = FileInfo {
       fileInfoLength = FileLength . fromIntegral $ BS.L.length bs
     , fileInfoHashes = Map.fromList [
-          (HashFnSHA256, Hash $ show (CH.hashlazy bs :: CH.Digest CH.SHA256))
+          (HashFnSHA256, Hash $ show $ Base16.encode $ SHA256.hashlazy bs)
         ]
     }
 

hackage-security/hackage-security.cabal
@@ -93,6 +93,7 @@ library
                        Prelude
   -- We support ghc 7.4 (bundled with Cabal 1.14) and up
   build-depends:       base              >= 4.5     && < 5,
+                       base16-bytestring >= 0.1.1   && < 0.2,
                        base64-bytestring >= 1.0     && < 1.1,
                        bytestring        >= 0.9     && < 0.11,
                        Cabal             >= 1.14    && < 1.26,

commit 64800b8787f256cd966d57ad58ec5308b7b3859c

Fix breakage introduced by 9b6adacb98a363708c1dd4e517ef6fc420fea8a0
The previous code used

    show (CH.hashlazy bs :: CH.Digest CH.SHA256))

but contrary to my assumption, the `Show` instance for `Digest` results in an
unquoted `String`. So when switching to the non-overloaded API, we need to use
ByteString's `unpack` rather than `show` to convert to a `String`

hackage-security/src/Hackage/Security/Key.hs
@@ -32,6 +32,7 @@ import Text.JSON.Canonical
 import qualified Crypto.Hash.SHA256   as SHA256
 import qualified Crypto.Sign.Ed25519  as Ed25519
 import qualified Data.ByteString      as BS
+import qualified Data.ByteString.Char8 as BS.C8
 import qualified Data.ByteString.Base16 as Base16
 import qualified Data.ByteString.Lazy as BS.L
 
@@ -160,7 +161,7 @@ class HasKeyId key where
 
 instance HasKeyId PublicKey where
   keyId = KeyId
-        . show
+        . BS.C8.unpack
         . Base16.encode
         . SHA256.hashlazy
         . renderCanonicalJSON

hackage-security/src/Hackage/Security/TUF/FileInfo.hs
@@ -18,6 +18,7 @@ import qualified Crypto.Hash.SHA256   as SHA256
 import qualified Data.Map             as Map
 import qualified Data.ByteString.Base16 as Base16
 import qualified Data.ByteString.Lazy as BS.L
+import qualified Data.ByteString.Char8 as BS.C8
 
 import Hackage.Security.JSON
 import Hackage.Security.TUF.Common
@@ -59,7 +60,7 @@ fileInfo :: BS.L.ByteString -> FileInfo
 fileInfo bs = FileInfo {
       fileInfoLength = FileLength . fromIntegral $ BS.L.length bs
     , fileInfoHashes = Map.fromList [
-          (HashFnSHA256, Hash $ show $ Base16.encode $ SHA256.hashlazy bs)
+          (HashFnSHA256, Hash $ BS.C8.unpack $ Base16.encode $ SHA256.hashlazy bs)
         ]
     }
 